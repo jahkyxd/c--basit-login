3819672f510e55350db85037b54db97915fbb1b2
jahkyxd <82198115+jahkyxd@users.noreply.github.com>
2022-07-31 13:19:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace my_first_project
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Lütfen adınızı giriniz!");
+            string  name;
+            name=Console.ReadLine();
+            Console.WriteLine("Lütfen yaşınızı giriniz!");
+            string age;
+            age=Console.ReadLine();
+            Console.WriteLine("Hoşgeldiniz " + name + " | " + age);
+            
+        }
+    }
+}
